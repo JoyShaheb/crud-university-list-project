43e190ac93bcf5e65944520db1cd15bcc388efbb
joyshaheb <khondokoralam@gmail.com>
2023-05-29 14:03:30 +0600

    work in progress

--- server/index.js
+++ server/index.js
@@ -16,7 +16,10 @@ app.use("/api/university", UniversityRoute);
 const port = process.env.PORT || 6001;
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     console.log("MongoDB database connection established successfully")
   )
