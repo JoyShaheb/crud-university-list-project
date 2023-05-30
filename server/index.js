957a0115f3f267a716b0fd003e3ba0c323cb46b1
joyshaheb <khondokoralam@gmail.com>
2023-05-30 16:27:02 +0600

    work in progress

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_DB_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("Unable to connect to MongoDB", err));
+const port = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
-app.listen(5000, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => res.send("hello world"));
+
+app.listen(port, () => {
+  console.log("this is workinh");
+});
